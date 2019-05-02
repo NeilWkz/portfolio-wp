bd31d3f885f192dcf36da894736061e42f44ea7f
NeilWkz <neilross.wkzdesign@gmail.com>
2019-05-02 14:34:30 +0100

    updated blog layout

--- wp-content/themes/neil-ross/src/js/main.js
+++ wp-content/themes/neil-ross/src/js/main.js
@@ -225,45 +225,14 @@ $(document).ready(function() {
       return false;
     });
   }
-  // Hide Header on on scroll down
-  var didScroll;
-  var lastScrollTop = 0;
-  var delta = 5;
-  var navbarHeight = $("#nav-wrap").outerHeight();
-
-  $(window).scroll(function(event) {
-    didScroll = true;
-  });
-
-  setInterval(function() {
-    if (didScroll) {
-      hasScrolled();
-      didScroll = false;
-    }
-  }, 250);
-
-  function hasScrolled() {
-    var st = $(this).scrollTop();
-
-    // Make sure they scroll more than delta
-    if (Math.abs(lastScrollTop - st) <= delta) return;
-
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight) {
-      // Scroll Down
-      $("#nav-wrap")
-        .removeClass("nav-down")
-        .addClass("nav-up");
-    } else {
-      // Scroll Up
-      if (st + $(window).height() < $(document).height()) {
-        $("#nav-wrap")
-          .removeClass("nav-up")
-          .addClass("nav-down");
-      }
-    }
-
-    lastScrollTop = st;
-  }
+ $("#nav-main").waypoint(
+   function(direction) {
+     if (direction == "down") {
+       $("#nav-wrap").addClass("nav-up");
+     } else {
+       $("#nav-wrap").removeClass("nav-up");
+     }
+   },
+ 
+ );
 });
